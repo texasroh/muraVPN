d206583535463403769dd48cf932412070919030
June Roh <texasroh@gmail.com>
2022-11-23 01:50:10 -0500

    add howtouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import Howtouse from "./components/Howtouse";
 
 function App() {
-    return (
-        <div style={{ height: "200vh" }}>
-            <Header />
-            <Banner />
-        </div>
-    );
+  return (
+    <div style={{ height: "200vh" }}>
+      <Header />
+      <Banner />
+      <Howtouse />
+    </div>
+  );
 }
 
 export default App;
